170ef2e711548c8d285aa1e38d0bd69d72cc6ced
alan <tecnofrostz@gmail.com>
2026-06-25 15:18:57 -0300

    restruturação do projeto

--- codigos/livro.cpp
+++
@@ -1,6 +0,0 @@
-#include "livro.h"
-
-Livro::Livro() : id(0), titulo(""), autor(""), emprestado(false) {}
-
-Livro::Livro(int i, const std::string& t, const std::string& a)
-    : id(i), titulo(t), autor(a), emprestado(false) {}
