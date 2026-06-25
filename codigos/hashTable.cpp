5ae393381acc94e24cac9326d3b2e72c8ab3c545
alan <tecnofrostz@gmail.com>
2026-06-25 16:11:41 -0300

    hashTable.cpp finalizado

--- codigos/hashTable.cpp
+++ codigos/hashTable.cpp
@@ -56,4 +56,48 @@ void HashTable::buscar(int matricula) {
 
     std::cout << "Aluno nao encontrado.\n";
 }
- 
+
+void HashTable::remover(int matricula) {
+
+    int indice = funcaoHash(matricula);
+
+    for(auto it = tabela[indice].begin();
+        it != tabela[indice].end();
+        ++it) {
+
+        if(it->getMatricula() == matricula) {
+
+            tabela[indice].erase(it);
+
+            std::cout
+                << "Aluno removido com sucesso!\n";
+
+            return;
+        }
+    }
+
+    std::cout << "Aluno nao encontrado.\n";
+}
+
+void HashTable::imprimir() {
+
+    std::cout
+        << "\n===== TABELA HASH =====\n";
+
+    for(int i = 0; i < TAM; i++) {
+
+        std::cout << "[" << i << "] -> ";
+
+        for(const auto& aluno : tabela[i]) {
+
+            std::cout
+                << "("
+                << aluno.getMatricula()
+                << ", "
+                << aluno.getNome()
+                << ") -> ";
+        }
+
+        std::cout << "NULL\n";
+    }
+}
